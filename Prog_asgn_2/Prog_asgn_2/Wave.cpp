99b9dbe43439b962bdad9d0a3e389f1250a8d1a3
Nick Earle <ncearle@gmail.com>
2018-10-26 15:28:39 -0700

    Paper and updates

--- Prog_asgn_2/Prog_asgn_2/Wave.cpp
+++ Prog_asgn_2/Prog_asgn_2/Wave.cpp
@@ -1,1 +1,14 @@
+#define _USE_MATH_DEFINES
 #include <iostream>
+#include <math.h>
+#include <fstream>
+#include<vector>
+
+using namespace std;
+
+constexpr int imax = 101
+constexpr double xmax = 1.0
+constexpr double dx = xmax / (imax - 1)
+
+
+
